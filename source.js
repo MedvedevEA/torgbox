90893c9980c1d4db7610dea4fb6b9a751857a221
iMedvedevEA <iMedvedevEA@gmail.com>
2024-11-01 00:09:12 +0700

    ver-2

--- source.js
+++ source.js
@@ -1,40 +1,11 @@
 module.exports = source;
 
-const months={
-    "янв":"01","фев":"02","мар":"03","апр":"04","мая":"05","июн.":"06","июл.":"07","авг":"08","сен":"09","окт":"10","ноя":"11","дек":"12",
-}
-const rules = [
-    [
-        {template:/(\d{4})[-.](\d{2})[-.](\d{2})/,toIso:(result)=>{return `${result[1]}-${result[2]}-${result[3]}` }},
-        {template:/(\d{2})[-./](\d{2})[-./](\d{4})/,toIso:(result)=>{return `${result[3]}-${result[2]}-${result[1]}` }},
-        {template:/["«]?(\d{1,2})["»]? ([а-я]{3,})\.? (\d{4})/,toIso:(result)=>{return `${result[3]}-${months[result[2].slice(0,3)]}-${result[1].padStart(2,"0")}` }},
-        {template:/.?/,toIso:(result)=>{return `0000-00-00`}},
-    ],
-    [
-        {template:/T(\d{2}):(\d{2}):(\d{2})\.(\d{3})/,toIso:(result)=>{return `${result[0]}`}},
-        {template:/T(\d{2}):(\d{2}):(\d{2})/,toIso:(result)=>{return `${result[0]}.000`}},
-        {template:/[^+](\d{2}):(\d{2})/,toIso:(result)=>{return `T${result[1]}:${result[2]}:00.000`}},
-        {template:/.?/,toIso:(result)=>{return `T00:00:00.000`}}
-    ],
-    [
-        {template:/\+(\d{2}):(\d{2})/,toIso:(result)=>{return `${result[0]}`}},
-        {template:/.?/,toIso:(result)=>{return `Z`}}
-    ]
-]
+const regExp=/\D*(\d{4}|\d{2}|\d)[^а-я\d]*(?:([а-я]{3,})|(\d{2}))\D*(\d{4}|\d{2})[^+\d]*(\d{2}:\d{2})?[^+\d]*(\d{2})?[^+\d]*(\d{3})?[^+]*(\+\d{2}:\d{2})?/i
+const months={"янв":"01","фев":"02","мар":"03","апр":"04","мая":"05","июн.":"06","июл.":"07","авг":"08","сен":"09","окт":"10","ноя":"11","дек":"12"}
 
 function source(obj) {
     str=obj.src[obj.options]
-    var response=""
-    rules.forEach ( (ruleGroup) =>{
-        var rule;
-        for (rule of ruleGroup) {
-            result=rule.template.exec(str)
-            if (result) {
-                response+=rule.toIso(result)
-                break
-            }
-        }
-    } )
-    return response
+    r=regExp.exec(str)
+    return `${r[1].length == 4 ? r[1] : r[4]}-${r[3] ?? months[r[2].slice(0,3)]}-${r[4].length < 3 ? r[4] : r[1].padStart(2,"0")}T${r[5]??"00:00"}:${r[6]??"00"}.${r[7]??"000"}${r[8]??"Z"}`
 }
 
